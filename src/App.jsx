bef437ba62d6f6236a9d692813a2c73b8c8e2708
Hasan Mohamed <hassanmo2363@gmail.com>
2025-04-18 01:37:15 +0200

    Remove unused vite.svg import

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import Home from "./Componant/Home/Home";
 import Protofolio from "./Componant/Protofolio/Protofolio";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import About from "./Componant/About/About";
 import Contact from "./Componant/Contact/Contact";
-import Navbar from "./Componant/Navbar/Navbar";
 import Layout from "./Componant/Layout/Layout";
 import NotFound from "./Componant/NotFound/NotFound";
 
 let routers = createBrowserRouter([
   {
-    path: "/",
+    path: "/first-project/",
     element: <Layout />,
     children: [
       { index: true, element: <Home /> },
       { path: "protofolio", element: <Protofolio /> },
       { path: "about", element: <About /> },
       { path: "contact", element: <Contact /> },
-      { path: "*", element: <NotFound/> },
+      { path: "*", element: <NotFound /> },
     ],
   },
 ]);
